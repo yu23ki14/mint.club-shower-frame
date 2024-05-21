45a2f06f16bae95fcbe4a9bf0981eb5b512f0ec6
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-05-21 23:03:03 +0900

    add japan

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,68 +1,291 @@
 /** @jsxImportSource frog/jsx */
 
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
+import { Button, Frog } from "frog"
+import { devtools } from "frog/dev"
+import { serveStatic } from "frog/serve-static"
 // import { neynar } from 'frog/hubs'
-import { handle } from 'frog/next'
-import { serveStatic } from 'frog/serve-static'
+import { handle } from "frog/next"
+import { validateAction } from "../../lib/neynar"
+import {
+  getAirdrop,
+  getAirdropIds,
+  getMerkleProof,
+  isAmountLeft,
+  isClaimed,
+  isEligible,
+} from "../../lib/mintclub"
+import JSONBig from "json-bigint"
+import { Address, formatEther } from "viem"
+import { MERKLE_DELIVERY_ABI } from "../../abi"
+import { APP_URL, MERKLE_DISTRIBUTE_CONTRACT } from "../../config"
 
-const app = new Frog({
-  assetsPath: '/',
-  basePath: '/api',
-  // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
+type State = {
+  airdrops: string
+  currentIndex: number
+  walletAddress: Address
+}
+
+export const app = new Frog<{ State: State }>({
+  assetsPath: "/",
+  basePath: "/api",
+  browserLocation: "https://nippon.cash",
+  initialState: {
+    airdrops: "",
+    currentIndex: 0,
+  },
 })
 
-// Uncomment to use Edge Runtime
-// export const runtime = 'edge'
-
-app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+app.frame("/", (c) => {
   return c.res({
     image: (
       <div
         style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
+          alignItems: "center",
+          backgroundImage: `url(${APP_URL}/original.jpg)`,
+          backgroundSize: "80% 100%",
+          backgroundPosition: "center 10%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
         }}
       >
         <div
           style={{
-            color: 'white',
+            color: "white",
             fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
             lineHeight: 1.4,
             marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+            backgroundColor: "#bc002d",
           }}
         >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+          $JAPAN Shower
         </div>
       </div>
     ),
-    intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
-    ],
+    imageAspectRatio: "1:1",
+    intents: [<Button action="/airdrop">Check Eligibility</Button>],
+  })
+})
+
+app.frame("/airdrop", async (c) => {
+  let walletAddress = c.previousState.walletAddress
+
+  if (!walletAddress) {
+    const {
+      trustedData: { messageBytes },
+    } = await c.req.json()
+    const validatedData = await validateAction(messageBytes)
+    walletAddress = validatedData.action.interactor.verified_addresses
+      .eth_addresses[0] as Address
+  }
+  if (!walletAddress) {
+    return c.res({
+      image: (
+        <div
+          style={{
+            alignItems: "center",
+            backgroundImage: `url(${APP_URL}/not-eligible.jpg)`,
+            backgroundSize: "80% 100%",
+            backgroundPosition: "center 10%",
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            height: "100%",
+            justifyContent: "center",
+            textAlign: "center",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              color: "white",
+              fontSize: 40,
+              fontStyle: "normal",
+              letterSpacing: "-0.025em",
+              lineHeight: 1.4,
+              marginTop: 500,
+              padding: "0 220px",
+              whiteSpace: "pre-wrap",
+              backgroundColor: "#bc002d",
+            }}
+          >
+            No More Airdrops
+          </div>
+        </div>
+      ),
+      imageAspectRatio: "1:1",
+      intents: [
+        <Button.Link href="https://mint.club/token/base/%F0%9F%87%AF%F0%9F%87%B5">
+          Buy $JAPAN
+        </Button.Link>,
+      ],
+    })
+  }
+
+  const airdropIds = await getAirdropIds()
+
+  const airdrops = (
+    await Promise.all(
+      airdropIds.map(async (id) => {
+        const airdrop = await getAirdrop(id)
+        const beforeEnd = airdrop.endTime > Date.now() / 1000
+        if (!beforeEnd) return { id, active: false, airdrop }
+        const eligible =
+          airdrop.merkleRoot ===
+          "0x0000000000000000000000000000000000000000000000000000000000000000"
+            ? true
+            : await isEligible(walletAddress, airdrop, id)
+        const claimed = await isClaimed(walletAddress, id)
+        const amountLeft = await isAmountLeft(id)
+
+        return {
+          id,
+          active: eligible && !claimed && amountLeft,
+          airdrop,
+        }
+      })
+    )
+  )
+    .filter((a) => a.active)
+    .sort((a, b) => a.airdrop.endTime - b.airdrop.endTime)
+  const currentIndex = c.previousState.currentIndex
+
+  c.deriveState((prevState) => {
+    prevState.airdrops = JSONBig.stringify(airdrops)
+    prevState.walletAddress = walletAddress
+    prevState.currentIndex += 1
+  })
+
+  if (airdrops.length === 0 || !airdrops[currentIndex]) {
+    return c.res({
+      image: (
+        <div
+          style={{
+            alignItems: "center",
+            backgroundImage: `url(${APP_URL}/not-eligible.jpg)`,
+            backgroundSize: "80% 100%",
+            backgroundPosition: "center 10%",
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            height: "100%",
+            justifyContent: "center",
+            textAlign: "center",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              color: "white",
+              fontSize: 40,
+              fontStyle: "normal",
+              letterSpacing: "-0.025em",
+              lineHeight: 1.4,
+              marginTop: 500,
+              padding: "0 220px",
+              whiteSpace: "pre-wrap",
+              backgroundColor: "#bc002d",
+            }}
+          >
+            No More Airdrops
+          </div>
+        </div>
+      ),
+      imageAspectRatio: "1:1",
+      intents: [
+        <Button.Link href="https://mint.club/token/base/%F0%9F%87%AF%F0%9F%87%B5">
+          Buy $JAPAN
+        </Button.Link>,
+      ],
+    })
+  } else {
+    const airdrop = airdrops[currentIndex]
+    return c.res({
+      image: (
+        <div
+          style={{
+            alignItems: "center",
+            backgroundImage: `url(${APP_URL}/original.jpg)`,
+            backgroundSize: "80% 100%",
+            backgroundPosition: "center 10%",
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            height: "100%",
+            justifyContent: "center",
+            textAlign: "center",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              color: "#bc002d",
+              fontSize: 30,
+              fontStyle: "normal",
+              letterSpacing: "-0.025em",
+              lineHeight: 1.4,
+              marginTop: 30,
+              padding: "20px 320px 0",
+              whiteSpace: "pre-wrap",
+              backgroundColor: "#ffffffad",
+            }}
+          >
+            {`for ${airdrop.airdrop.title}`}
+          </div>
+          <div
+            style={{
+              color: "#bc002d",
+              fontSize: 50,
+              fontStyle: "normal",
+              letterSpacing: "-0.025em",
+              lineHeight: 1.4,
+              padding: "0 320px",
+              whiteSpace: "pre-wrap",
+              backgroundColor: "#ffffffad",
+              fontWeight: "bold",
+            }}
+          >
+            {`${Math.ceil(
+              Number(formatEther(airdrop.airdrop.amountPerClaim))
+            )} $JAPAN`}
+          </div>
+        </div>
+      ),
+      imageAspectRatio: "1:1",
+      intents: [
+        <Button.Transaction action="/airdrop" target="/claim">
+          Claim
+        </Button.Transaction>,
+        <Button action="/airdrop">Next</Button>,
+      ],
+    })
+  }
+})
+
+app.transaction("/claim", async (c) => {
+  const { airdrops, currentIndex } = c.previousState
+  const airdrop = JSONBig.parse(airdrops)[currentIndex - 1]
+
+  const merkleProof =
+    airdrop.airdrop.merkleRoot ===
+    "0x0000000000000000000000000000000000000000000000000000000000000000"
+      ? []
+      : await getMerkleProof(c.previousState.walletAddress, airdrop.airdrop)
+
+  return c.contract({
+    abi: MERKLE_DELIVERY_ABI,
+    functionName: "claim",
+    args: [airdrop.id, merkleProof],
+    chainId: "eip155:8453",
+    to: MERKLE_DISTRIBUTE_CONTRACT,
   })
 })
 
---
+++ app/config.ts
@@ -0,0 +1,5 @@
+export const APP_URL = process.env.APP_URL as string
+export const TOKEN_ADDRESS = process.env.TOKEN_ADDRESS as `0x${string}`
+export const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY as string
+export const MERKLE_DISTRIBUTE_CONTRACT = process.env
+  .MERKLE_DISTRIBUTE_CONTRACT as `0x${string}`
